3f50bbef033498cc0e8731ecaf78587457530849
Jack Baum <jackbaum@umich.edu>
2026-02-04 21:28:17 -0500

    set up for render deployment

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def order_book(meal):
     return jsonify(MarketService.get_order_book(meal))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
